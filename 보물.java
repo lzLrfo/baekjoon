694e9364497cf0336cbe34235902feef7cc16da9
lzLrfo <rydhd11@gmail.com>
2022-01-12 20:37:24 +0900

    Create 보물.java
    https://www.acmicpc.net/problem/1026

---
+++ 보물.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] a = new int[n];
+        int[] b = new int[n];
+        int s = 0;
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for(int i = 0; i < n; i++)
+            a[i] = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine(), " ");
+        for(int i = 0; i < n; i++)
+            b[i] = Integer.parseInt(st.nextToken());
+        Arrays.sort(a);
+        Arrays.sort(b);
+        for(int i = 0; i < n; i++) {
+            s += a[i] * b[n - 1 -i];
+        }
+        System.out.println(s);
+    }
+}
