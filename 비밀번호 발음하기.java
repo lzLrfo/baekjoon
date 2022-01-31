2690d2f3b6ef5f843d0b5bc62f6f367a2863d9ec
lzLrfo <rydhd11@gmail.com>
2022-01-31 22:49:55 +0900

    Create 비밀번호 발음하기.java

---
+++ 비밀번호 발음하기.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        while(true) {
+            String pwd = br.readLine();
+            if(pwd.equals("end"))
+                break;
+            char pre;
+            for(int i = 0; i < pwd.length(); i++) {
+                
+            }
+        }
+    } 
+}
