a8df1826353752737e061f765e2e8f550110a5a4
lzLrfo <rydhd11@gmail.com>
2022-02-04 00:14:26 +0900

    Update 피보나치 함수.java
    단순 재귀가 아닌 다이나믹 프로그래밍으로 푸는 문제

--- 피보나치 함수.java
+++ 피보나치 함수.java
@@ -3,30 +3,24 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    static int zero;
-    static int one;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine());
+        int[] n = new int[t];
+        for(int i = 0; i < t; i++)
+            n[i] = Integer.parseInt(br.readLine());
+        int[] zero = new int[41];
+        int[] one = new int[41];
+        zero[0] = 1; zero[1] = 0;
+        one[0] = 0; one[1] = 1;
+        for(int i = 2; i < 41; i++) {
+            zero[i] = zero[i-1] + zero[i-2];
+            one[i] = one[i-1] + one[i-2];
+        }
         StringBuilder sb = new StringBuilder();
         for(int i = 0; i < t; i++) {
-            zero = 0;
-            one = 0;
-            int n = Integer.parseInt(br.readLine());
-            fibonacci(n);
-            sb.append(zero).append(" ").append(one).append("\n");
+            sb.append(zero[n[i]]).append(" ").append(one[n[i]]).append("\n");
         }
         System.out.println(sb);
-    }
-    static int fibonacci(int n) {
-        if (n == 0) {
-            zero++;
-            return 0;
-        } else if (n == 1) {
-            one++;
-            return 1;
-        } else {
-            return fibonacci(n-1) + fibonacci(n-2);
-        }
     }
 }
